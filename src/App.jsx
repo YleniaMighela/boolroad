7293b75544bcfcc173b2433e3138ca8960bb0a60
Salvatore <workstudioms@gmail.com>
2025-03-17 18:00:00 +0100

    TravelsDetailCard creation and basic css style and layout of the card

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import TravelsDetailCard from "./components/TravelsDetailCard"
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+// import HomePage from "./pages/HomePage"
 
 function App() {
 
   return (
-    <>
-      <h1>Boolroad</h1>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+
+      <Route path="/" element={<HomePage />}/>
+
+      <Route path="/travel" element={<TravelsDetailCard />} />
+      
+      </Route>
+
+    </Routes>
+    </BrowserRouter>
+
   )
 }
 
