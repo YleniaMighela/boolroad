6afa396c989b5dbe484c1b4b5efb2a9d1bbdd918
Giovanni <dibellogio@gmail.com>
2025-03-17 18:03:42 +0100

    add bootstrap Travels list

--- src/components/TravelsList.jsx
+++ src/components/TravelsList.jsx
@@ -2,15 +2,24 @@
 
 import travels from "../data/DB";
 
+// importo bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
 export default function TravelsList() {
 
     return (
-        <ul>
-            {travels.map((trip) => (
-                <li key={trip.id}>
-                    {trip.citta} - {trip.dataPartenza} - {trip.dataArrivo}
-                </li>
-            ))}
-        </ul>
+
+        <div className="container mt-4">
+            <h2 className="mb-3">Destinazioni di Viaggio</h2>
+            <ul className="list-group">
+                {travels.map((trip) => (
+                    <li key={trip.id} className="list-group-item d-flex justify-content-between align-items-center">
+                        Viaggio: {trip.citta} - Date: {trip.dataPartenza} - {trip.dataArrivo}
+                        <button className="btn btn-primary">Dettagli</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+
     );
 };
