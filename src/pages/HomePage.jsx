2399f54905acaf4af0be3115c1133da58fddf1b1
Giovanni <dibellogio@gmail.com>
2025-03-17 17:24:30 +0100

    add HomePage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+// HomePage.jsx
+
+// importo il componente TravelList
+import TravelList from "../components/TravelList"
+
+function App() {
+
+    return (
+        <>
+            <TravelList />
+        </>
+    )
+}
+
+export default App
