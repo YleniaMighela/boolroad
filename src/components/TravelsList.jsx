05e8ce26ccb44175c65eb981777b0fa109648ad8
Giovanni <dibellogio@gmail.com>
2025-03-17 17:45:00 +0100

    fix travels list

--- src/components/TravelsList.jsx
+++ src/components/TravelsList.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import { travelData } from "../data/travelData";
+//  TravvelsList.jsx
+
+import travels from "../data/DB";
 
 export default function TravelsList() {
 
     return (
         <ul>
-            {travelData.map((trip) => (
+            {travels.map((trip) => (
                 <li key={trip.id}>
                     {trip.citta} - {trip.dataPartenza} - {trip.dataArrivo}
                 </li>
