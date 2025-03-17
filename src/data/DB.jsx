23a08df9034f43be7032e2dfe00214d3e42409f8
Luca Belardinelli <belardinelliluca@gmail.com>
2025-03-17 16:48:06 +0100

    creation DB file

---
+++ src/data/DB.jsx
@@ -0,0 +1,35 @@
+const viaggi = [
+    {
+        id: 1,
+        citta: "Parigi",
+        dataPartenza: "2025-04-10",
+        dataArrivo: "2025-04-15"
+    },
+    {
+        id: 2,
+        citta: "Londra",
+        dataPartenza: "2025-05-05",
+        dataArrivo: "2025-05-12"
+    },
+    {
+        id: 3,
+        citta: "Roma",
+        dataPartenza: "2025-06-01",
+        dataArrivo: "2025-06-07"
+    },
+    {
+        id: 4,
+        citta: "Tokyo",
+        dataPartenza: "2025-07-20",
+        dataArrivo: "2025-08-05"
+    },
+    {
+        id: 5,
+        citta: "New York",
+        dataPartenza: "2025-09-10",
+        dataArrivo: "2025-09-18"
+    }
+];
+
+
+export default viaggi;
