86bbce0c933724af7c40a7ebfff8cda6d5ba2044
Ylenia <yleniamighela@gmail.com>
2025-03-17 17:45:46 +0100

    rename class in className

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,8 +7,8 @@ export default function Header() {
 
     return (
         <header>
-            <nav class="navbar bg-body-tertiary">
-                <div class="container-fluid">
+            <nav className="navbar bg-body-tertiary">
+                <div className="container-fluid">
                     <Link to="/"> <h1 id="title">BOOLROAD</h1>
                         <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24" />
                     </Link>
