98605abd1e033281f73e4ab7163db840480feabb
Giovanni <dibellogio@gmail.com>
2025-03-18 13:16:59 +0100

    fix search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,12 +8,11 @@ export default function SearchBar({ search, setSearch }) {
     };
 
     return (
-        <nav className="navbar bg-body-tertiary">
-            <div className="container-fluid">
+        <nav className="navbar">
+            <div className="container-fluid justify-content-center">
                 <form className="d-flex" role="search">
-                    <input className="form-control me-2" type="search" placeholder="Search"
+                    <input className="form-control me-2" type="search" placeholder="Cerca partecipante..."
                         aria-label="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
-                    <button className="btn btn-outline-success" type="submit" onClick={handleChange}>Search</button>
                 </form>
             </div>
         </nav>
