f3f2cbc7ff1dcd216d9d81f1a8b58a6ed608f309
Ylenia <yleniamighela@gmail.com>
2025-03-18 09:54:14 +0100

    ritorno commit Alberto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,6 @@ export default function Header() {
 
     return (
         <header>
-
-
-
             <nav className="d-flex justify-content-between align-items-center navbar p-3" style={{ backgroundColor: "#0044cc" }}>
                 <div className="d-flex align-items-center">
                     <Link to="/" className="d-flex align-items-center text-decoration-none" style={{ color: "#28a745" }}>
