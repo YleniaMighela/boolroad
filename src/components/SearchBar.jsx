d6ddb00391a54d427fc4c8b9227453da100b757c
Salvatore <workstudioms@gmail.com>
2025-03-18 15:27:18 +0100

    SearchBar function testing and fix

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,7 +12,7 @@ export default function SearchBar({ search, setSearch }) {
             <div className="container-fluid justify-content-center">
                 <form className="d-flex" role="search">
                     <input className="form-control me-2" type="search" placeholder="Cerca partecipante..."
-                        aria-label="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
+                        aria-label="Search" value={search} onChange={handleChange} />
                 </form>
             </div>
         </nav>
